ed156310c8bc3810fcef1980c0a714d0bf17ebf2
satyasrinivaskola <satyakola333@gmail.com>
2026-09-05 21:51:00 +0530

    url updated as render trail

--- server.js
+++ server.js
@@ -257,7 +257,10 @@ const role = "student";
             password,
             10
         );
-
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PORT:", process.env.DB_PORT);
 
         // Insert user
         const [result] = await pool.execute(
